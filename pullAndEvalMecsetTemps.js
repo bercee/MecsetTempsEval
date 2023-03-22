7ca5676c591c05c8035876cec1e24d5d2547ffc6
Bertalan Kovacs <bkovacs@chemaxon.com>
2023-03-22 10:09:31 +0100

    print temps sorted by timestamp

---
+++ pullAndEvalMecsetTemps.js
@@ -0,0 +1,27 @@
+import promptSync from 'prompt-sync';
+
+const prompt = promptSync();
+
+const API_KEY = prompt("Please give me the API key for the MecsetTemps GET API: ");
+console.log(`API_KEY=${API_KEY}`);
+
+const URL = 'https://u2x3hurv1e.execute-api.eu-central-1.amazonaws.com/default/temperature/get-data';
+
+async function getData() {
+    const response = await fetch(URL,
+        {
+            method: "GET",
+            headers: {
+                "Content-Type": "application-json",
+                "x-api-key": API_KEY
+            }
+        });
+
+    return response.json();
+}
+
+const rawData = await getData();
+const body = JSON.parse(rawData.body);
+const items = body.Items;
+items.sort((a,b) => new Date(a.timestamp) - new Date(b.timestamp));
+items.forEach(i => console.log(i.temp));
