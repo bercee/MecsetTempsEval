e5a686d777ea701055a27aa136caac711dcbf168
Bertalan Kovacs <bkovacs@chemaxon.com>
2023-03-24 12:47:41 +0100

    minor additions in logging and prompt input

--- pullAndEvalMecsetTemps.js
+++ pullAndEvalMecsetTemps.js
@@ -2,7 +2,7 @@ import promptSync from 'prompt-sync';
 
 const prompt = promptSync();
 
-const API_KEY = prompt("Please give me the API key for the MecsetTemps GET API: ");
+const API_KEY = prompt("Please give me the API key for the MecsetTemps GET API: ",undefined, {sigint: true});
 console.log(`API_KEY=${API_KEY}`);
 
 const URL = 'https://u2x3hurv1e.execute-api.eu-central-1.amazonaws.com/default/temperature/get-data';
@@ -24,4 +24,4 @@ const rawData = await getData();
 const body = JSON.parse(rawData.body);
 const items = body.Items;
 items.sort((a,b) => new Date(a.timestamp) - new Date(b.timestamp));
-items.forEach(i => console.log(i.temp));
+items.forEach(i => console.log(`${i.timestamp}, ${i.temp}`));
